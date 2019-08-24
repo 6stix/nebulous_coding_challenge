4812673ce02d789b3b10afb826c97671d3748f02
nm <nmaini1@swarthmore.edu>
2019-08-23 21:51:47 -0400

    Included memoized solution

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 This program aims to solve Nebulous' coding challenge. The instructions to the challenge
 can be found here: https://docs.google.com/document/d/1eurMI2z6JIfHM9zPg4gWz5NzB8TQxOmSAE4GN_3C8co/edit
+
+The general idea of the program is to take two generators with their respective
+passed-in constants and generate numbers up to a defined limit. Each step involves
+generating a number and then bitwise ANDing the number with the bit_match integer.
+If, during a single step, both of the generators bitwise ANDed numbers match, the
+match_count is incremented.
+
+Note: this program is extremely easy to expand upon. For example, if a list was
+desired instead of a count, one could create a list of tuples of matching generated
+numbers and then return the list.
+
+Date: August 23rd, 2019
--- generators.py
+++ generators.py
@@ -1,16 +1,9 @@
 """
-This program aims to solve Nebulous' coding challenge. The instructions to the challenge
-can be found here: https://docs.google.com/document/d/1eurMI2z6JIfHM9zPg4gWz5NzB8TQxOmSAE4GN_3C8co/edit
+This program aims to solve Nebulous' coding challenge.
 
 The general idea of the program is to take two generators with their respective
-passed-in constants and generate numbers up to a defined limit. Each step involves
-generating a number and then bitwise ANDing the number with the bit_match integer.
-If, during a single step, both of the generators bitwise ANDed numbers match, the
-match_count is incremented.
-
-Note: this program is extremely easy to expand upon. For example, if a list was
-desired instead of a count, one could create a list of tuples of matching generated
-numbers and then return the list.
+passed-in constants, generate numbers up to a defined limit, and return/print the
+number of matched numbers (when bitwise ANDed with a bit_match integer).
 
 Date: August 23rd, 2019
 """
